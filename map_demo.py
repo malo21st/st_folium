b90ae5392a1643cca74da5cb0a07577f1bf6f3f2
malo21st <44739330+malo21st@users.noreply.github.com>
2021-03-29 19:10:52 +0900

    Update map_demo.py

--- map_demo.py
+++ map_demo.py
@@ -7,6 +7,9 @@ import pandas as pd
 
 # コンビニの位置データ
 df_shops = pd.read_json('shop.json')
+shop_lst = []
+for _, pos in df_shops.iterrows():
+    shop_lst.append((pos['lat'], pos['lon']))
 
 # ウォーキングのトレースデータ
 gpx_file = open('track-2690684.gpx')
@@ -23,30 +26,25 @@ df_gpx = pd.DataFrame(walk_lst, columns=['lat', 'lon'])
 center_lat = (df_gpx['lat'].min() + df_gpx['lat'].max())/2
 center_lon = (df_gpx['lon'].min() + df_gpx['lon'].max())/2
 
-page = st.radio(
+# サイドメニュー
+page = st.sidebar.radio(
     'Select map type',
     ['Marker', 'Poly Line', 'MarkerCluster'],
     index = 0
     )
 
 # 地図作成
+m = folium.Map(location=[center_lat, center_lon], zoom_start=12)
 if page == 'Marker':
-    m = folium.Map(location=[center_lat, center_lon], zoom_start=12)
-    for _, pos in df_shops.iterrows():
-        folium.Marker((pos['lat'], pos['lon']), icon=folium.Icon(color='blue')).add_to(m)
+    folium.Marker(shop_lst, icon=folium.Icon(color='blue')).add_to(m)
         
 elif page == 'Poly Line':
-    m = folium.Map(location=[center_lat, center_lon], zoom_start=12)
     folium.PolyLine(
         locations = walk_lst
     ).add_to(m)
 
 elif page == 'MarkerCluster':
-    m = folium.Map(location=[center_lat, center_lon], zoom_start=12)
-    locations = []
-    for _, pos in df_shops.iterrows():
-        locations.append((pos['lat'], pos['lon']))
-    plugins.MarkerCluster(locations).add_to(m)
+    plugins.MarkerCluster(shop_lst).add_to(m)
 
 # call to render Folium map in Streamlit
 folium_static(m)
