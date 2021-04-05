77907b31a1b49865e6d0d359ff88117dad2011e8
malo21st <44739330+malo21st@users.noreply.github.com>
2021-04-06 00:31:50 +0900

    Update map_demo.py

--- map_demo.py
+++ map_demo.py
@@ -55,8 +55,8 @@ elif page == 'Poly Line':
     ).add_to(m)
     folium.TopoJson(
         data=open('41201.topojson', encoding='utf-8'),
-#         object_path='41201.topojson',
-#         name="topojson"
+        object_path='41201.topojson',
+        name="topojson"
     ).add_to(m)
     msg = "線も引けちゃうよ"
 
